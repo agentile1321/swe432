96536bf38320d78072eedde708b652ec5c2183ab
agentile1321 <65682605+agentile1321@users.noreply.github.com>
2020-10-16 13:24:53 -0400

    Update Assignment5.java

--- src/main/java/servlet/Assignment5.java
+++ src/main/java/servlet/Assignment5.java
@@ -43,7 +43,7 @@ public class Assignment5 extends HttpServlet{
 		
 		
 		//Parse it into a structure that separates boolean variables and logical operators
-		String[] legalOps = {"&&", "AND", "&", "^", "||", "|", "OR", "V", "~", "NOT", "!", "==", "=", "EQUAL"};
+		ArrayList legalOps = new ArrayList(Arrays.asList("&&", "AND", "&", "^", "||", "|", "OR", "V", "~", "NOT", "!", "==", "=", "EQUAL"));
 		ArrayList arrayVars = new ArrayList(Arrays.asList(logicalOperation.split(" "))); //split by space "A & B -> [A,&,B]"
 		ArrayList arrayOps = new ArrayList();
 		
