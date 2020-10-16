4e438ef3282dcdaf151f6997b800c83fed59c67f
agentile1321 <65682605+agentile1321@users.noreply.github.com>
2020-10-16 13:01:54 -0400

    Update Assignment5.java

--- src/main/java/servlet/Assignment5.java
+++ src/main/java/servlet/Assignment5.java
@@ -43,7 +43,7 @@ public class Assignment5 extends HttpServlet{
 		
 		
 		//Parse it into a structure that separates boolean variables and logical operators
-		var arrayVars = logicalOperation.split(" "); //split by space "A & B -> [A,&,B]"
+		String[] arrayVars = logicalOperation.split(" "); //split by space "A & B -> [A,&,B]"
 		ArrayList arrayOps = new ArrayList();
 		// var opIndex = array.indexOf("&") //get index of operators
 		//get & into its own array
