8144f16cd21158be62bff42e7c255b489d2feef2
agentile1321 <65682605+agentile1321@users.noreply.github.com>
2020-11-24 20:05:42 -0500

    Update assignment8.java

--- src/main/java/servlet/assignment8.java
+++ src/main/java/servlet/assignment8.java
@@ -17,12 +17,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.RequestDispatcher;
 import javax.xml.stream.XMLEventFactory;
 import javax.xml.stream.XMLEventWriter;
 import javax.xml.stream.XMLEventReader;
