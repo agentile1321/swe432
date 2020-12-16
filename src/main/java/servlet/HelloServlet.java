362c79417eadc4d59533d0351147e894ba911bf5
agentile1321 <65682605+agentile1321@users.noreply.github.com>
2020-12-15 22:27:35 -0500

    Update HelloServlet.java

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -27,51 +27,39 @@ public class HelloServlet extends HttpServlet
 
         out.println ("<HTML>");
         out.println ("<HEAD>");
-        out.println ("<TITLE>A simple servlet program</TITLE>");
+        out.println ("<TITLE>SWE 432 Final Exam</TITLE>");
         out.println ("</HEAD>");
 
         out.println ("<BODY>");
         out.println ("<CENTER>");
 
-        out.println (" <B>Hello!</B><BR> <!--  English -->");
-        out.println (" <B>Alo!</B><BR> <!--  Portuguese -->");
-        out.println (" <B>Anyong haseyo!</B><BR> <!--  Korean -->");
-        out.println (" <B>Apa Kabar!</B><BR> <!--  Indonesian -->");
-        out.println (" <B>Ave!</B><BR> <!--  Latin -->");
-        out.println (" <B>Bon jour!</B><BR> <!--  French -->");
-        out.println (" <B>Ciao!</B><BR> <!--  Italian -->");
-        out.println (" <B>Hajur!</B><BR> <!--  Nepali (India) -->");
-        out.println (" <B>Hallo!</B><BR> <!--  German -->");
-        out.println (" <B>Hej!</B><BR> <!--  Swedish -->");
-        out.println (" <B>Hei!</B><BR> <!--  Norwegian -->");
-        out.println (" <B>Hola!</B><BR> <!--  Spanish -->");
-        out.println (" <B>Kaise ho!</B><BR> <!--  Hindi (depending on who you believe) -->");
-        out.println (" <B>Kem Chho, Kem Che!</B><BR> <!--  Gujurati (India) -->");
-        out.println (" <B>Ki Khobor!</B><BR> <!--  Bengali -->");
-        out.println (" <B>Marhaba!</B><BR> <!--  Arabic -->");
-        out.println (" <B>Moin Moin!</B><BR> <!--  German, Hamburg -->");
-        out.println (" <B>Moni!</B><BR> <!--  Chichewa -->");
-        out.println (" <B>Namaskaram!</B><BR> <!--  Telugu (India) -->");
-        out.println (" <B>Namskar!</B><BR> <!--  Hindi -->");
-        // Why do I keep getting different words in Hindi?
-        // out.println (" <B>Namaste!</B><BR> <!--  Hindi -->");
-        // out.println (" <B>Namastar!</B><BR> <!--  Hindi -->");
-        out.println (" <B>Konnichiwa!</B><BR> <!--  Japanese -->");
-        out.println (" <B>Manh gioi!</B><BR> <!--  Vietnamese -->");
-        out.println (" <B>Marhabai!</B><BR> <!--  Arab -->");
-        out.println (" <B>Ni hao!</B><BR> <!--  Chinese -->");
-        out.println (" <B>Shalom!</B><BR> <!--  Hebrew -->");
-        out.println (" <B>Sallam!</B><BR> <!--  Persian -->");
-        out.println (" <B>Sat-Sri-Akal!</B><BR> <!--  Punjabi (India) -->");
-        out.println (" <B>Strasvedte!</B><BR> <!--  Russian -->");
-        out.println (" <B>Yah-shimu-siz!</B><BR> <!--  Uighur -->");
-        out.println (" <B>Zhao  Shen!</B><BR> <!-- Cantonese -->");
+        out.println ("<h1>SWE 432 Final Exam </h1>");
+        out.println ("<span style=\"width: 90%; align: left\">");
+        out.println ("<span>Please enter comma separated strings or numbers you would like to sort</span><br/>")
+        out.println ("<input type=\"text\" id=\"inputBox\" name=\"inputBox\" style=\"align: left; width: 60%\"/> &nbsp;&nbsp;&nbsp;<button id=\"buttonSort\" onclick=\"defaultSort()\">Sort</button>");
+        out.println ("</span>");
+        out.println ("<br /><br />);
+        out.println ("<span id=\"result\" style=\"width: 90%; align: left\"></span>");
+        out.println ("<script type=\"text/javascript\">");
+        out.println ("var resultTxt = \"\";");
+        out.println ("function defaultSort() {");
+        out.println ("document.getElementById(\"result\").innerHTML = \"\";");
+        out.println ("var str = document.getElementById(\"inputBox\").value;")
+        out.println ("var res = str.split(",");");
+        out.println ("var trimedArr = res.map(str => str.trim());");
+        out.println ("var sortedArr = trimedArr.sort();");
+        out.println ("sortedArr.forEach(concatinateFunction);");
+        out.println ("var sortedResult = resultTxt.substring(0, resultTxt.length - 2);");
+       out.println (" document.getElementById(\"result\").innerHTML = sortedResult;");
+     out.println ("}");
 
-        out.println ("<P>");
-        out.println ("For a longer listing of \"hellos\" to the world, go see");
-        out.println ("<A HREF=\"http://www.ipl.org/youth/hello/\">http://www.ipl.org/youth/hello/</A>");
+    out.println (" numbers.forEach(myFunction);");
 
-        out.println ("</CENTER>");
+    out.println (" function concatinateFunction(value, index, array) {");
+         out.println ("resultTxt = resultTxt + value + \", \";");
+    }
+ out.println ("</script>");
+      
         out.println ("</BODY>");
 
         out.println ("</HTML>");
